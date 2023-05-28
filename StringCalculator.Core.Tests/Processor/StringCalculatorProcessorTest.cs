7722592243ef268867a0115cb2286d5413bc0b18
RanaSalameh <rana.salameh313@gmail.com>
2023-05-28 23:10:54 +0300

    "the simplest thing" step

---
+++ StringCalculator.Core.Tests/Processor/StringCalculatorProcessorTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using StringCalculator;
+
+namespace StringCalculator.Core.Processor
+{
+    public class StringCalculatorProcessorTest
+    {
+        //private readonly calculator = new StringCalculatorProcessor();
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void Add_ValidInputStringOfNumbers_ReturnsCorrectSum(String numbers, int expectedSum)
+        {
+
+            //Arange
+            var calculator = new StringCalculatorProcessor();
+
+            //Act
+            var actualSum = calculator.Add(numbers);
+
+            //Assert
+            Assert.Equal(expectedSum, actualSum);
+        }
+
+        [Theory]
+        [InlineData("1,2,3")]
+        [InlineData("1,2,3,4,5")]
+        public void Add_InvalidInputString_ThrowsArgumentException(string numbers)
+        {
+            //Arange
+            var calculator = new StringCalculatorProcessor();
+
+            //Assert
+            Assert.Throws<System.ArgumentException>(() => { calculator.Add(numbers); });
+
+        }
+    }
+}
---
+++ StringCalculator/Processor/StringCalculatorProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringCalculator.Core.Processor
+{
+    public  class StringCalculatorProcessor
+    {
+        public StringCalculatorProcessor()
+        {
+        }
+
+        public  int Add(string numbers)
+        {
+            if (numbers.Equals(""))
+                return 0;
+
+            string[] numbersArray = numbers.Split(',');
+
+            if (numbersArray.Length > 2)
+                throw new System.ArgumentException("Invalid input: More than 2 numbers are not allowed.");
+
+            int sum = 0;
+            foreach (var numberString in numbersArray)
+            {
+                sum += int.Parse(numberString);
+            }
+            return sum;
+
+        }
+    }
+}
